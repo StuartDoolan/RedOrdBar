9b51d210e60aaa798e10e860281dc14c4a81c066
StuartDoolan <stuartdoolan08@gmail.com>
2016-10-19 01:46:04 +0100

    first full attempt
    With orrections to formatting file readings suggested by matt it now basically works. Output data not in best form and due some cleanup but functional just now

--- initbarydraft2.py
+++ initbarydraft2.py
@@ -1,74 +1,89 @@
 import numpy as np
 
-efile = 'earth00-19-DE405.dat'
-fp1 = open(efile,'r') #opens data file
- 
+# read in file
+f = open('earth00-19-DE405.dat', 'r')
 if False:
     print('Error, could not open Earth ephemeris file');   # displays error message
-else:  print ('yay!') # so file opened
-
-for line in fp1:
-  if line.startswith('#'): # skips comment lines
-    continue
-    lines = fp1.readline() # should read all lines except comment
-    break #### doesnt work at all, will just pretend to have data...
-  
-# read first line, check right number of values and assign names to those entries
-#line1vals = array of values
+filecontents = f.readlines()
+f.close()
 
 
-ephemEentries = 6 # temp fill
+# skip through header lines starting with '#'
+filestart = 0
+while 1:
+    if filecontents[filestart][0] == '#':
+        filestart += 1
+    else:
+        break
 
-ephemEpos = np.zeros((ephemEentries,3))   # allocate memory for eph info
-ephemEvel = np.zeros((ephemEentries,3))
-ephemEacc = np.zeros((ephemEentries,3))
+# find number of entries in the file
+nentries = int((filecontents[filestart].split()[2]).strip())
 
-ephemEgps = np.zeros((ephemEentries,1))
-print(ephemEpos)
+# create array for data (nentries rows by 10 columns)
+ephemdata = np.zeros((nentries, 10)) 
 
-#read remaining lines - Use np.loadtxt( 10 columns of ephemEentries length) will create array
+# read in the actual data
+for i in range(nentries):
+    thisline = []
+    for j in range(4):
+        lines = filecontents[filestart+1+(i*4)+j].split()
+        for val in lines:
+            thisline.append(float(val.strip()))
+    ephemdata[i,:] = np.array(thisline)
 
-# name variables apropriate columns of above array
+#assign each variable
+[ephemEgps, xpos, ypos, zpos, velx, vely, velz, accx, accy, accz] = [ephemdata[:,0], ephemdata[:,1], ephemdata[:,2], ephemdata[:,3], ephemdata[:,4], ephemdata[:,5], ephemdata[:,6], ephemdata[:,7], ephemdata[:,8], ephemdata[:,9]]
+ephemEpos = [xpos, ypos, zpos]
+ephemEvel = [ velx, vely, velz]
+ephemEacc = [accx, accy, accz]
 
-fp1.close() #closes fp1
-# del(line1vals)
+[Einitgps, Edttables, Eentries] = filecontents[22].split() ### cheating mode, FIX!
+Eephem = [ephemEgps, Edttables, Eentries, ephemEpos, ephemEvel, ephemEacc]
 
 
-####### now repeat pretty much whole process for sun file
+####### Sun Stuff
 
-sfile = 'sun00-19-DE405.dat'
-fp1 = open(sfile,'r') #opens data file
- 
+# read in file
+f = open('sun00-19-DE405.dat', 'r')
 if False:
     print('Error, could not open Sun ephemeris file');   # displays error message
-else:  print ('yay!') # so file opened
-
-for line in fp1:
-  if line.startswith('#'): # skips comment lines
-    continue
-    lines = fp1.readline() # should read all lines except comment
-    break #### doesnt work at all, will just pretend to have data...
-  
-# read first line, check right number of values and assign names to those entries
-#line1vals = array of values
+filecontents = f.readlines()
+f.close()
 
 
-ephemSentries = 6 # temp fill
+# skip through header lines starting with '#'
+filestart = 0
+while 1:
+    if filecontents[filestart][0] == '#':
+        filestart += 1
+    else:
+        break
 
-ephemSpos = np.zeros((ephemSentries,3))   # allocate memory for eph info
-ephemSvel = np.zeros((ephemSentries,3))
-ephemSacc = np.zeros((ephemSentries,3))
+# find number of entries in the file
+nentries = int((filecontents[filestart].split()[2]).strip())
 
-ephemSgps = np.zeros((ephemSentries,1))
+# create array for data (nentries rows by 10 columns)
+ephemdata = np.zeros((nentries, 10)) 
+
+# read in the actual data
+for i in range(nentries):
+    thisline = []
+    for j in range(4):
+        lines = filecontents[filestart+1+(i*4)+j].split()
+        for val in lines:
+            thisline.append(float(val.strip()))
+    ephemdata[i,:] = np.array(thisline)
+
+#assign each variable
+[ephemSgps, xpos, ypos, zpos, velx, vely, velx, accx, accy, accz] = [ephemdata[:,0], ephemdata[:,1], ephemdata[:,2], ephemdata[:,3], ephemdata[:,4], ephemdata[:,5], ephemdata[:,6], ephemdata[:,7], ephemdata[:,8], ephemdata[:,9]]
+
+ephemSpos = [xpos, ypos, zpos]
+ephemSvel = [ velx, vely, velz]
+ephemSacc = [accx, accy, accz]
 
 
-#read remaining lines - Use np.loadtxt( 10 columns of ephemEentries length) will create array
+[Sinitgps, Sdttables, Sentries] = filecontents[22].split()
 
-# name variables apropriate columns of above array
-
-fp1.close() #closes fp1
-
-
-
+Sephem = [ephemSgps, Sdttables, Sentries, ephemSpos, ephemSvel, ephemSacc] ### gives all data as a structured list (probably not perfect)
         
 
